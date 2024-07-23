8a605613f12dab226c39b3dc1a4eca51d76acb87
Marc Troelitzsch <marc.troelitzsch@gmail.com>
2024-07-23 14:56:25 +0200

    refactor: replace thiserror with own implementation

--- git-foreach/src/error.rs
+++ git-foreach/src/error.rs
@@ -1,26 +1,19 @@
 use std::path::PathBuf;
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug)]
 pub enum Error {
-    #[error("run_command failed for {path}")]
     CommandExecutionFailed { path: PathBuf },
-
-    #[error("run_command failed with non-zero exit code for {path}")]
     CommandExecutionFailedWithNonZeroExitCode { path: PathBuf, exit_code: i32 },
-
-    #[error("error walking directory: {0}")]
-    Walk(#[from] git_walk::Error),
-
-    #[error("{0}")]
-    InvalidUsage(#[from] clap::Error),
+    Walk(git_walk::Error),
+    InvalidUsage(clap::Error),
 }
 
 impl Error {
     /// Get the exit code for the error.
     fn get_exit_code(&self) -> i32 {
         match self {
-            Error::CommandExecutionFailedWithNonZeroExitCode { exit_code, .. } => *exit_code,
-            Error::InvalidUsage(source) => source.exit_code(),
+            Self::CommandExecutionFailedWithNonZeroExitCode { exit_code, .. } => *exit_code,
+            Self::InvalidUsage(source) => source.exit_code(),
             _ => 1,
         }
     }
@@ -29,5 +22,47 @@ impl Error {
     pub fn print_and_exit(&self) {
         eprintln!("{self}");
         std::process::exit(self.get_exit_code());
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::CommandExecutionFailed { path } => {
+                write!(f, "run_command failed for {}", path.display())
+            }
+            Self::CommandExecutionFailedWithNonZeroExitCode { path, exit_code } => {
+                write!(
+                    f,
+                    "run_command failed with non-zero exit code {} for {}",
+                    exit_code,
+                    path.display()
+                )
+            }
+            Self::Walk(source) => write!(f, "error walking directory: {source}"),
+            Self::InvalidUsage(source) => write!(f, "{source}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Walk(source) => Some(source),
+            Self::InvalidUsage(source) => Some(source),
+            _ => None,
+        }
+    }
+}
+
+impl From<git_walk::Error> for Error {
+    fn from(source: git_walk::Error) -> Self {
+        Self::Walk(source)
+    }
+}
+
+impl From<clap::Error> for Error {
+    fn from(source: clap::Error) -> Self {
+        Self::InvalidUsage(source)
     }
 }
--- git-walk/src/error.rs
+++ git-walk/src/error.rs
@@ -1,9 +1,30 @@
-/// Error type for git-walk.
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("failed to walk directory")]
-    Walk(#[from] ignore::Error),
-}
-
 /// Result type for git-walk.
 pub type Result<T> = std::result::Result<T, Error>;
+
+/// Error type for git-walk.
+#[derive(Debug)]
+pub enum Error {
+    Walk(ignore::Error),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Walk(source) => write!(f, "error walking directory: {source}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Walk(source) => Some(source),
+        }
+    }
+}
+
+impl From<ignore::Error> for Error {
+    fn from(source: ignore::Error) -> Self {
+        Self::Walk(source)
+    }
+}
